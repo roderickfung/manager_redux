2acac546ee51ca38cde82b29f08409db296b64b9
Roderick <roderickfung@gmail.com>
2017-03-02 15:25:20 -0800

    user authentication dispatched

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import AuthReducer from './AuthReducer';
 
 export default combineReducers({
   // dummy reducer as placeholder
-  placeholder: () => []
+  // placeholder: () => []
+  auth: AuthReducer
 });
