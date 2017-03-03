ad28c37bde844be65ebc96ac0e501aeea55fa63f
Roderick <roderickfung@gmail.com>
2017-03-02 22:39:30 -0800

    redux thunk

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Text } from 'react-native';
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 
 class LoginForm extends Component {
 
@@ -17,6 +17,17 @@ class LoginForm extends Component {
   onButtonPress() {
     const { email, password } = this.props;
     this.props.loginUser ({ email, password });
+  }
+
+  renderButton() {
+    if (this.props.loading) {
+      return <Spinner size='small'/>;
+    }
+    return (
+      <Button onPress={ this.onButtonPress.bind(this) }>
+        Login
+      </Button>
+    );
   }
 
   render() {
@@ -44,9 +55,7 @@ class LoginForm extends Component {
           { this.props.error }
         </Text>
         <CardSection>
-          <Button onPress={ this.onButtonPress.bind(this) }>
-            Login
-          </Button>
+          { this.renderButton() }
         </CardSection>
       </Card>
     );
@@ -62,8 +71,8 @@ const styles = {
 }
 
 const mapStateToProps = ({ auth }) => {
-  const { email, password, error } = auth;
-  return { email: email, password: password, error: error };
+  const { email, password, error, loading } = auth;
+  return { email: email, password: password, error: error, loading };
 }
 
 export default connect( mapStateToProps, {
